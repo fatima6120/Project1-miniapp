19fb3dd6c06c94f184225f3f8796e1608efffe69
Fatima Alarmali <fatimaalarmali2004@gmail.com>
2025-11-19 22:12:00 +0200

    fixing images url

--- lib/store.dart
+++ lib/store.dart
@@ -30,14 +30,26 @@ class Store {
 
 List<Store> items = [
 
-  Store('iPhone 14', 300, 'https://example.com/iphone.png', 'Phone'),
-  Store('Samsung Galaxy S23', 700, 'https://example.com/galaxy.png','Phone'),
-  Store('Google Pixel 8', 600, 'https://example.com/pixel.png', 'Phone'),
+  Store('iPhone 14', 300,
+      'https://images.unsplash.com/photo-1678685888221-cda773a3dcdb?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      , 'Phone'),
+  Store('Samsung Galaxy S23', 700,
+      'https://images.unsplash.com/photo-1709744722656-9b850470293f?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      ,'Phone'),
+  Store('Google Pixel 8', 600,
+      'https://images.unsplash.com/photo-1697355360151-2866de32ad4d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      , 'Phone'),
 
 
-  Store('MacBook Pro', 1999, 'https://example.com/macbook.png','Laptop'),
-  Store('Dell XPS 13', 1299, 'https://example.com/dell.png','Laptop'),
-  Store('HP Spectre x360', 1399, 'https://example.com/hp.png','Laptop'),
+  Store('MacBook Pro', 1999,
+      'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?q=80&w=1026&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      ,'Laptop'),
+  Store('Dell XPS 13', 1299,
+      'https://images.unsplash.com/photo-1720556405438-d67f0f9ecd44?q=80&w=830&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      ,'Laptop'),
+  Store('HP Spectre x360', 1399,
+      'https://images.unsplash.com/photo-1589561084283-930aa7b1ce50?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      ,'Laptop'),
 ];
 class ShowSelectedItems extends StatelessWidget {
   const ShowSelectedItems({required this.width, required this.category, Key? key}) : super(key: key);
