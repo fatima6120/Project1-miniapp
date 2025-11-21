518cfa895f7c7ed1c9b5d5b11f81eb9ccac008e5
Fatima Alarmali <fatimaalarmali2004@gmail.com>
2025-11-21 19:43:31 +0200

    editing the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My Shop App',
-      theme: ThemeData(primarySwatch: Colors.teal),
-      home: const Home(),
+      home: Home(),
     );
   }
 }
